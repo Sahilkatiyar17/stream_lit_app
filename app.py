70d05f88dd1cfd87a0fced316419a7dba7bd6b74
Sahilkatiyar17 <sahilkatiyar2024@gmail.com>
2025-05-31 22:46:05 +0530

    added the file of app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+
+st.write("Hello World")
+st.write("Hello World")
+st.write("Hello World")
+st.write("Hello World")
+if st.button("Click me"):
+    st.write("You clicked me!")
+
